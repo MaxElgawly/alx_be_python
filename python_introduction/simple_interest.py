3ed7f0c605fa4bb20cee8b2badb48a56b67f9a32
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-07-12 02:14:53 +0300

    Add files via upload

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#defining varaibles
+principle = 1000
+rate = 0.05
+time = 3
+
+#calcuating interest formula
+interest = principle * rate * time 
+
+#print interest output 
+print(f"the simple interest is: {interest}")
