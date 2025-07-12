00f71a68834defb62a825399d57da6b4976369a2
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-07-12 09:58:17 +0300

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -2,7 +2,7 @@
 currentAge = input("How old are you?")
 
 #calcuating how old in 2050
-newAge = currentAge + 50
+newAge = int(currentAge) + int(27)
 
 #displaying new age
 print(f"In 2050, you will be {newAge} years old")
