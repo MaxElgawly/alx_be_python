c13825011398166a5ecd841d6418d0f3002e21d5
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-07-12 09:55:11 +0300

    Add files via upload

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+#Prompt the user to input their current age
+currentAge = input("How old are you?")
+
+#calcuating how old in 2050
+newAge = currentAge + 50
+
+#displaying new age
+print(f"In 2050, you will be {newAge} years old")
