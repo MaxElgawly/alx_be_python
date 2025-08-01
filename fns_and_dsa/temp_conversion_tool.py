ff2ea8eccc40e241fd729de6ddcc6209a1b41c64
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-08-02 01:57:27 +0300

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,4 +1,3 @@
-# Global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
@@ -31,4 +30,5 @@ def main():
         print("Invalid temperature. Please enter a numeric value.")
 
 if __name__ == "__main__":
+
     main()
